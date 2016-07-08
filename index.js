4ce65065cc235930ff1d8dfcaefc00009238deec
C. Thomas Bailey <christopher.thomas.bailey@gmail.com>
2016-07-08 19:32:09 -0400

    Try feeding the network's output back to itself.
    Sounds a little crunchier and thicker. Not unpleasant.

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ const freq = 200
 const sampleCount = 10 * constants.sampleRate
 const experiment = synth.generate(freq, sampleCount, 30)
 const control = tone.generate(freq, sampleCount, 30)
-const error = experiment.map((val, i) => experiment[i] - control[i])
+// const error = experiment.map((val, i) => experiment[i] - control[i])
 
 console.log('Experiment complete:')
 // console.log(`Control data: ${JSON.stringify(control, null, 2)}`)
 // console.log(`Experiment data: ${JSON.stringify(experiment, null, 2)}`)
-console.log(`Error: ${JSON.stringify(error, null, 2)}`)
+// console.log(`Error: ${JSON.stringify(error, null, 2)}`)
 
 console.log(`Saving .csv files`)
 csv.save(experiment, './data/synth-c-major')
